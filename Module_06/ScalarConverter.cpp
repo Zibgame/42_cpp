75bf20223df1f7cb2a884e54cb4fb5d1a67a5d75
Zibrian Cadinot <zcadinot@c2r5p3.42lehavre.fr>
2026-04-07 16:12:07 +0200

    Add module 6

---
+++ Module_06/ScalarConverter.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 16:09:19 by zcadinot          #+#    #+#             */
+/*   Updated: 2026/04/07 16:11:50 by zcadinot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+void ScalarConverter::convert(const std::string& literal)
+{
+    (void)literal;
+}
---
+++ Module_06/ScalarConverter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 16:09:22 by zcadinot          #+#    #+#             */
+/*   Updated: 2026/04/07 16:11:38 by zcadinot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class ScalarConverter
+{
+private:
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter&);
+    ScalarConverter& operator=(const ScalarConverter&);
+    ~ScalarConverter();
+
+public:
+    static void convert(const std::string& literal);
+};
